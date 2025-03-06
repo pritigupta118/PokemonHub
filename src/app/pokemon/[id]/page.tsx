f9ce11302c36b941ed238b5d06c5992f62844952
Priti Gupta <civilstudent9900@gmail.com>
2025-03-06 22:29:10 +0530

    Refactor Pokemon page component to improve naming consistency and simplify function definition

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -1,25 +1,14 @@
-import DetailedPokemonCard from '@/components/DetailedPokemonCard'
-import { fetchPokemonById } from '@/lib/api'
+import PokemonDetail from "@/components/DetailedPokemonCard"
+import { fetchPokemonById } from "@/lib/api"
 
-import { notFound } from 'next/navigation'
-import React from 'react'
+import { notFound } from "next/navigation"
 
-export interface SearchParams {
-  id: string
-}
-
-const Page =async ({
-  params,
-}: {
-  params: SearchParams
-}) => {
+export default async function PokemonPage({ params }: { params: { id: string } }) {
   const pokemon = await fetchPokemonById(Number.parseInt(params.id))
 
-   if(!pokemon){
+  if (!pokemon) {
     notFound()
-   }
+  }
 
-   return <DetailedPokemonCard pokemon={pokemon}/>
+  return <PokemonDetail pokemon={pokemon} />
 }
-
-export default Page
